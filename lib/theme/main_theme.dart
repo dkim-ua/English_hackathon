4fb37c287ab5c0df13e7e1f91cbc5c1c769419fa
aVOKADO-hub <65965051+aVOKADO-hub@users.noreply.github.com>
2024-04-22 12:54:07 +0300

    Designed HomePage

--- lib/theme/main_theme.dart
+++ lib/theme/main_theme.dart
@@ -3,3 +3,6 @@ import 'package:flutter/material.dart';
 var mainColor = const Color(0xff173373).withOpacity(0.65);
 var backgroundColor = Colors.white;
 var helpColor = const Color(0xff173373).withOpacity(0.30);
+var lessons_color = const Color(0xff173373);
+var iconChecked = Image.asset('lib/images/checked.png');
+var iconUnchecked = Image.asset('lib/images/unchecked.png');
