08c668f591436667e271e6ff6dc133cfb6616922
dkim-ua <dkim.ua@gmail.com>
2024-12-06 14:13:51 +0200

    kursach

--- lib/class/constant.dart
+++ lib/class/constant.dart
@@ -3,4 +3,4 @@ import 'package:english_hakaton/class/voise_assistant_tts.dart';
 List<String> languages = ['en-US', 'uk-UA'];
 bool isVoiceAssistant = false;
 VoiceAssistantTextToSpeech voiceAssistantTextToSpeech = VoiceAssistantTextToSpeech();
-const String baseIP = '91.199.45.37:7050';
+const String baseIP = '194.26.232.76:7050';
